c153483f7b7a638727dbb16cca5f2dfbace94de1
Felipe <111323311+felipebvasco@users.noreply.github.com>
2022-08-15 22:06:32 -0300

    Update BetGame.py

--- BetGame.py
+++ BetGame.py
@@ -18,7 +18,14 @@ saldo = float(input('Coloque o Saldo Disponível para começar as apostas: '))
 option = int(input('''Digite qual opção você deseja utilizar:
 1 - Apostar em Número específico (increase balance value by 83,4%)
 2 - Apostar no Sistema High-Low (increase balance value by 20%)
+3 - Sair
+'''))
 
+def opcao():
+    option = int(input('''Digite qual opção você deseja utilizar:
+1 - Apostar em Número específico (increase balance value by 83,4%)
+2 - Apostar no Sistema High-Low (increase balance value by 20%)
+3 - Sair
 '''))
 
         
@@ -32,9 +39,10 @@ def game1():
     numerodado = random.randint(1, 6)
     print('O número sorteado foi {}'.format(numerodado))
     if numerodado == escolhaplayer:
-        saldopremio = saldorestante + (saldoaposta * 1.834)      
+        saldopremio = saldorestante + (saldoaposta * 1.834)
+        saldopremio
         print('''Parabéns, Você acertou o número sorteado!
-Saldo atual: {:.2f}'''.format(saldopremio))
+Saldo atual: R${:.2f}'''.format(saldopremio))
     if escolhaplayer != numerodado:
         saldoperdido = saldo - saldoaposta
         print('Você errou, que pena! Seu saldo se encontra com o valor de R${:.2f}'.format(saldoperdido))
@@ -58,22 +66,23 @@ def game2():
         if numerodado2 in low:
             saldopremio1 = saldorestante + (saldoaposta * 1.2)
             print('''Parabéns, O número sorteado foi {} e estava na sua escolha (low)
-Saldo atual: {}'''.format(numerodado2, saldopremio1))
-        if escolhaplayer1 != numerodado2:
+Saldo atual: R${:.2f}'''.format(numerodado2, saldopremio1))
+        if numerodado2 not in low:
             saldoperdido1 = saldo - saldoaposta
             print('''Você errou, que pena!
-Saldo atual: {}'''.format(saldoperdido1))
+Saldo atual: R${:.2f}'''.format(saldoperdido1))
     if escolhaplayer1 == 2:
         escolhatipo2 = high = [4, 5, 6]
         numerodado2 = random.randint(1, 6)
         if numerodado2 in high:
             saldopremio1 = saldorestante + (saldoaposta * 1.2)
             print('''Parabéns, O número sorteado foi {} e estava na sua escolha (high)
-Saldo atual: {}'''.format(numerodado2, saldopremio1))
+Saldo atual: R${:.2f}'''.format(numerodado2, saldopremio1))
         if numerodado2 not in high:
             saldoperdido2 = saldo - saldoaposta
             print('''Você errou, que pena!
-Saldo atual: {}'''.format(saldoperdido2))
+Saldo atual: R${:.2f}'''.format(saldoperdido2))
+
    
 if option == 1:
     game1()
@@ -92,7 +101,8 @@ Deseja continuar Jogando?
 2 - Não
 
 '''))
-        
+    if continuar == 2:
+        opcao()
         
     if saldo == 0:
         print('Seu dinheiro acabou')
@@ -115,14 +125,16 @@ Deseja continuar Jogando?
 2 - Não
 
 '''))
+    if continuar == 2:
+        opcao()
 
-    else:
-        exit()
+if option == 3:
+    exit()
 
 
 if saldo == 0:
     print('Seu dinheiro acabou')
-    
+    exit()
     
 
         
